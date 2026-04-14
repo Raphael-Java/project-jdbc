1462b848d73ee921a496ea35d258a476c0fffd5f
Raphael-Mendess <raphael.hkiller@gmail.com>
2026-04-14 19:06:39 -0300

    Creating SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.util.Date;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,6 +11,7 @@ public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
 		Seller seller = new Seller(21, "Raphael", "raphael.hpod@gmail.com", new Date(), 3000.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}
